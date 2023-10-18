3727664e705723de24e00deed6149f1300c86988
cgaspard3333 <clement.gaspard@u-bordeaux.fr>
2023-10-18 16:51:05 +0200

    update train.sh

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ killall -9 python
 
 for env in $ENVS;
 do
-	#nohup python train.py --env $env-v0 --algo td3 --num-threads 4 --track > $env.log &
-	python train.py --env $env-v0 --algo td3 --num-threads 4 --track
+	#nohup python train.py --env $env-v0 --algo td3 --num-threads 4 --track --gym-packages gym_footsteps_planning > $env.log &
+	python train.py --env $env-v0 --algo td3 --num-threads 4 --track --gym-packages gym_footsteps_planning
 done
 
