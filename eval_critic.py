10c93c70e728681c134dea574505cc0bbbd2568c
cgaspard3333 <cgaspard3333@gmail.com>
2024-02-12 16:20:17 +0100

    bugfix eval-critic

--- eval_critic.py
+++ eval_critic.py
@@ -17,10 +17,28 @@ folder = "logs"
 reset_dict_list = []
 more_steps_array = np.array([])
 
-nb_tests = 100
+nb_tests = 1000
 
+foot_length = 0.14
+foot_width = 0.08
 radius_arround_obstacle = 0.3
 obstacle_coordinates = [0.3, 0]
+
+def in_obstacle(foot_pose, obstacle_radius):
+    in_obstacle = False
+    cos_theta = np.cos(foot_pose[2])
+    sin_theta = np.sin(foot_pose[2])
+    for sx in [-1, 1]:
+        for sy in [-1, 1]:
+            P_corner_foot = np.array([sx * foot_length / 2, sy * foot_width / 2])
+            P_corner_world = (
+                foot_pose[:2]
+                + P_corner_foot[0] * np.array([cos_theta, sin_theta])
+                + P_corner_foot[1] * np.array([-sin_theta, cos_theta])
+            )
+            if np.linalg.norm(P_corner_world - np.array([0.3, 0]), axis=-1) < obstacle_radius:
+                in_obstacle = True
+    return in_obstacle
 
 for i in range(nb_tests):
     # for theta in np.arange(0, 360, 22.5):
@@ -30,19 +48,20 @@ for i in range(nb_tests):
         "start_foot_pose": np.random.uniform([-2, -2, -math.pi], [2, 2, math.pi]),
         "start_support_foot": "left" if (np.random.uniform(0, 1) > 0.5) else "right",
         "target_foot_pose": None,
-        "target_support_foot": None,
+        "target_support_foot": "right" if (np.random.uniform(0, 1) > 0.5) else "left",
         "obstacle_radius":0.15,
     }
 
-    for theta in (45,0,-45):
-        for foot in ["left", "right"]:
-            x = obstacle_coordinates[0]+radius_arround_obstacle*np.cos(np.deg2rad(180-theta))
-            y = obstacle_coordinates[1]+radius_arround_obstacle*np.sin(np.deg2rad(180-theta))
+    while in_obstacle(reset_dict["start_foot_pose"], reset_dict["obstacle_radius"]):
+        reset_dict["start_foot_pose"] = np.random.uniform([-2, -2, -math.pi], [-2, 2, math.pi])
 
-            reset_dict["target_foot_pose"] = [x, y, np.deg2rad(-theta)]
-            reset_dict["target_support_foot"] = foot
+    for theta in (45,22.5,0,-22.5,-45):
+        x = obstacle_coordinates[0]+radius_arround_obstacle*np.cos(np.deg2rad(180-theta))
+        y = obstacle_coordinates[1]+radius_arround_obstacle*np.sin(np.deg2rad(180-theta))
 
-            reset_dict_angles = np.append(reset_dict_angles, reset_dict.copy())
+        reset_dict["target_foot_pose"] = [x, y, np.deg2rad(-theta)]
+
+        reset_dict_angles = np.append(reset_dict_angles, reset_dict.copy())
 
     reset_dict_list.append(reset_dict_angles)
 print(f"Reset Dict: {len(reset_dict_list)}")
@@ -107,5 +126,6 @@ for reset_dict_exp in tqdm(reset_dict_list):
 
 mean_more_steps = np.mean(more_steps_array)
 print(f"Mean More Steps: {mean_more_steps}, Pourcentage error: {more_steps_array.shape[0]*100/nb_tests}%")
+print(f"More Steps: {more_steps_array}")
 
     
