b9748cd88d7209bad1b6f4e236e895331869fea7
cgaspard3333 <cgaspard3333@gmail.com>
2024-02-23 16:20:23 +0100

    add metrics to compare_ROS_RL

--- compare_ROS_RL.py
+++ compare_ROS_RL.py
@@ -20,7 +20,14 @@ for nb_situation in range(0, 3):
 
     print(f"\n-----Situation: {nb_situation+1} with obstacle radius = {reset_dict_arr[0]['obstacle_radius']}-----")
     diff_nb_steps = nb_steps_arr_RL - nb_steps_arr_ROS
-    print(f"Nb steps difference: {diff_nb_steps}")
+
+    nb_positifs = np.sum(diff_nb_steps > 0)
+    pourcentage = (nb_positifs / len(diff_nb_steps)) * 100
+    
+    print(f"Mean number of steps ROS: {np.mean(nb_steps_arr_ROS)}")
+    print(f"Mean number of steps RL: {np.mean(nb_steps_arr_RL)}")
     print(f"Mean: {np.mean(diff_nb_steps)}")
+    print(f"Pourcentage RL better case: {100-pourcentage}%")
+    print(f"Nb steps difference: {diff_nb_steps}")
 
     mean_relative_error = ((nb_steps_arr_RL - nb_steps_arr_ROS)/nb_steps_arr_ROS)*100
