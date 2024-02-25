a29b37e1783b4744c97450d3dd55ae48bf423a27
cgaspard3333 <cgaspard3333@gmail.com>
2024-02-25 01:59:51 +0100

    Update test_one_value.py

--- test_one_value.py
+++ test_one_value.py
@@ -8,21 +8,38 @@ from rl_zoo3.utils import get_model_path
 from tqdm import tqdm
 import math
 
-env_name = "footsteps-planning-any-obstacle-multigoal-v0"
+env_name = "footsteps-planning-any-withball-v0"
 exp_nb = 0
 algo = "td3"
 
 folder = "logs"
 
 max_episode_len = 90
+obstacle_coordinates = [0.3, 0]
 
+def rotation_arround_obstacle(theta: float, center: list = obstacle_coordinates, radius: float = 0.3) -> list:
+    x = center[0] + radius * np.cos(np.deg2rad(180 - theta))
+    y = center[1] + radius * np.sin(np.deg2rad(180 - theta))
+    return [x, y, np.deg2rad(-theta)]
+
+#Catch eye figure situation
 reset_dict = {
-    "start_foot_pose": [-2.0, 0.0,  0.0],
+    "start_foot_pose": [1.0, -0.15, math.pi/4],
     "start_support_foot": "right",
-    "target_foot_pose": [0.0, 0.0, -0.0],
+    "target_foot_pose": [0.0, 0.0, 0.0],
     "target_support_foot": "right",
     "obstacle_radius":0.15,
 }
+
+reset_dict = {
+    "start_foot_pose": [1.0, -0.15, math.pi/4],
+    "start_support_foot": "right",
+    "target_foot_pose": [0.0, 0.0, 0.0],
+    "target_support_foot": "right",
+    "obstacle_radius":0.15,
+}
+
+reset_dict["target_foot_pose"] = rotation_arround_obstacle(45)
 
 print(f"Env. Name: {env_name}, Exp. Number: {exp_nb}, Algo: {algo}")
 
@@ -56,3 +73,6 @@ while (not done) & (total_step < max_episode_len):
     obs, reward, done, truncated, infos = env.step(action)
     total_step += 1
     total_reward += reward
+
+print(f"Total reward: {total_reward}, Total steps: {total_step}") 
+input()
